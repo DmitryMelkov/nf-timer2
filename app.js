af7e67186f770c3a09bcc12ca06ef49ebfc91697
Дмитрий <melkdim@list.ru>
2023-03-24 15:27:11 +0500

    add corrections

--- app.js
+++ app.js
@@ -104,3 +104,14 @@ modalMore.addEventListener('click', () => {
 modalClose.addEventListener('click', () => {
   modalWindow.classList.remove('open');
 });
+
+//card-block
+const myAtropos = Atropos({
+  el: '.card-block-js',
+  activeOffset: 10,
+  alwaysActive: true,
+  rotateXMax: 50,
+  rotateYMax: 50,
+  activeOffset: 30,
+
+});
